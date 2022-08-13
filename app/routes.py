19cd3362fbffd189ff966d010fd06a333682d9ab
omar nazih <omarnazeh295@gmail.com>
2022-08-13 19:53:41 +0200

    created shortlinks endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import jsonify, request
+from flask import request, json, jsonify
 
 
 @app.route('/')
@@ -7,20 +7,47 @@ def index():
     return {'status': 'OK',
             f'{request.url}shortlinks': 'Show all shortned links',
             f'{request.url}createshortlink': 'Create new shortlink',
-            f'{request.url}shortlinks/<slug>': 'Update shortlink <slug>'}
+            f'{request.url}shortlinks/<slug>': 'Update shortlink <slug>'}, 200
+
 
 @app.route('/shortlinks', methods=['GET'])
-def shortLinks():
-    return 'shortlinks'
+def shortLinks():    
+    shortlinks_db_list = []
+
+    # Return all shortlinks from mongodb
+    shortlinks = db.test.find({})
+    for entry in shortlinks:        
+        row = {
+            'slug': entry['slug'],
+            'link': entry['link'],
+        }
+        shortlinks_db_list.append(row)
+    
+    response = {
+        'data':shortlinks_db_list
+    }    
+        
+    return jsonify(response), 200
+    
+
 
 @app.route('/createshortlink', methods=['POST'])
-def createShortLink():
-    # db.test.insert_one({'slug': 's5G1f3', 'link': "https://jsfiddle.net/8vb2pqtm/"})
-    return 'createshortlink'
+def createShortLink():            
+    if not request.is_json:
+        return jsonify({"msg":"Missing JSON in request", "status":400}), 400
+        
+    req_data = request.get_json()
+    print(req_data)
 
+    return req_dat
+
+
+@app.route('/shortlinks', defaults={'slug':None}, methods=['PUT'])
 @app.route('/shortlinks/<slug>', methods=['PUT'])
-def updateShortLink(slug):
-    return f'updateShortLink {slug}'
+def updateShortLink(slug):    
+    # !NOTE Slug stays as it is
+    return f'update shortlink for this slug {slug}'
+
 
 @app.errorhandler(404)
 def not_found(error=None):
@@ -30,4 +57,14 @@ def not_found(error=None):
     }
     response = jsonify(message)
     response.status_code = 404
+    return response    
+
+@app.errorhandler(500)
+def not_found(error=None):
+    message = {
+        'message': 'Internal Server Error ' + request.url,
+        'status': 500
+    }
+    response = jsonify(message)
+    response.status_code = 500
     return response    
